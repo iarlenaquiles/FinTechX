aaca465e3883c8112d983a90d7062429f2523071
Iarlen Aquiles <iarlensilva775@gmail.com>
2025-06-27 00:20:02 -0300

    fix: settings

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,18 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
-from urllib.parse import quote_plus
-import os
 
-load_dotenv()
+from app.config import settings
 
-username = os.getenv("DATABASE_USERNAME", "user_read_only")
-password = quote_plus(os.getenv("DATABASE_PASSWORD", "laborit_teste_2789"))  # escapa caracteres especiais
-host     = os.getenv("DATABASE_HOST", "northwind-mysql-db.ccghzwgwh2c7.us-east-1.rds.amazonaws.com")
-port     = os.getenv("DATABASE_PORT", "3306")
-name     = os.getenv("DATABASE_NAME", "northwind")
+username = settings.DATABASE_USERNAME
+password = settings.DATABASE_PASSWORD
+host     = settings.DATABASE_HOST
+port     = settings.DATABASE_PORT
+name     = settings.DATABASE_NAME
 
-db_type  = os.getenv("DATABASE_TYPE", "mysql+pymysql")
+db_type  = settings.DATABASE_TYPE
 
 DATABASE_URL = f"{db_type}://{username}:{password}@{host}:{port}/{name}"
 
